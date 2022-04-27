a93d0ecee64292431bbd8afa43b562cf8d2a1959
Sory <sorinnoroc1@gmail.com>
2022-04-27 23:16:59 +0300

    First!

---
+++ app/src/main/java/com/soryKhan/callrecorder/broadcastreceiver/PhoneStateReceiver.kt
@@ -0,0 +1,13 @@
+package com.soryKhan.callrecorder.broadcastreceiver
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+abstract class PhoneStateReceiver : BroadcastReceiver() {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
